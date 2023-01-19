598287ee847c3e1ea468df529ba47d9e991e1c55
kamini chavan <kamini.chavan14@gmail.com>
2023-01-19 16:38:41 +0530

    Added sucessfully

---
+++ src/com/velocity/Demo.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	int no=5;
+	int fact=1;
+	for(int i=1;i<=no;i++) {
+		fact=fact*i;
+		System.out.println(fact);
+	}
+
+	}
+
+}
